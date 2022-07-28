b9b0e51e9fa0c3def2845232cd72db234bd6701d
sushrut <sushrut@sushrut-HB.local>
2022-07-28 14:47:20 +0530

    first commit - Project created

---
+++ MyChores/MyChores/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChores
+//
+//  Created by sushrut on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
